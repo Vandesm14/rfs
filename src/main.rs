b4cbb128d5f56b750032fe8a32970154ecebcd19
Shane <vandesm14@gmail.com>
2023-10-05 12:23:19 -0700

    create filesystem struct and hdd file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::Write;
+
+#[derive(Debug)]
+struct Filesystem {
+  path: String,
+  file: std::fs::File,
+}
+
+impl Filesystem {
+  fn new(path: &str) -> Self {
+    let mut file = std::fs::OpenOptions::new()
+      .create(true)
+      .write(true)
+      .open(path)
+      .unwrap();
+
+    Self {
+      path: path.to_string(),
+      file,
+    }
+  }
+
+  fn init(&mut self) {
+    let buf = [0u8; 1024];
+    self.file.write_all(&buf).unwrap();
+  }
+}
+
 fn main() {
-  println!("Hello World!");
+  let mut filesystem = Filesystem::new("harddrive.bin");
+
+  filesystem.init();
 }
