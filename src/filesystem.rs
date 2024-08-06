95891459e1a7aee2493e8e5c891e1b1d1fe8d0fe
Shane <vandesm14@gmail.com>
2024-08-06 13:09:28 -0400

    serialize main super block

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -1,5 +1,6 @@
 use std::io::{self, Read, Seek, Write};
 
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 pub trait BlockAlign {
@@ -19,24 +20,47 @@ pub trait BlockAlign {
   }
 }
 
-struct BlockKindMain;
+#[repr(C)]
+#[derive(
+  Debug, Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize,
+)]
+struct BlockKindMain {
+  free_header_ptr: usize,
+  free_title_ptr: usize,
+  free_data_ptr: usize,
+
+  /// This is unused but kept for alignment and for future use.
+  unused_ptr: usize,
+}
 impl BlockAlign for BlockKindMain {
   const SIZE: usize = 32;
   const COUNT: usize = 1;
 }
 
+#[repr(C)]
+#[derive(
+  Debug, Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize,
+)]
 struct BlockKindHeader;
 impl BlockAlign for BlockKindHeader {
   const SIZE: usize = 32;
   const COUNT: usize = 128;
 }
 
+#[repr(C)]
+#[derive(
+  Debug, Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize,
+)]
 struct BlockKindTitle;
 impl BlockAlign for BlockKindTitle {
   const SIZE: usize = 32;
   const COUNT: usize = 128;
 }
 
+#[repr(C)]
+#[derive(
+  Debug, Clone, Copy, PartialEq, Eq, Default, Serialize, Deserialize,
+)]
 struct BlockKindData;
 impl BlockAlign for BlockKindData {
   const SIZE: usize = 128;
@@ -101,28 +125,53 @@ where
 
   pub fn init(&mut self, size: usize) -> Result<(), BulkError> {
     self.inner.seek(std::io::SeekFrom::Start(0))?;
-    let buf = vec![0; size];
-    self.inner.write_all(&buf)?;
+    let mut cursor = 0;
+    let mut buf = vec![0; size];
 
     let mut needed_size = BlockKindMain::total_size();
     if size < needed_size {
       return Err(BulkError::TooSmallForMainSuperBlock);
     }
 
+    let mut main_super_block = BlockKindMain {
+      free_header_ptr: needed_size,
+      free_title_ptr: 0,
+      free_data_ptr: 0,
+      unused_ptr: 0,
+    };
+
     needed_size += BlockKindHeader::total_size();
     if size < needed_size {
       return Err(BulkError::TooSmallForHeaderSuperBlock);
     }
+
+    main_super_block.free_header_ptr = needed_size;
 
     needed_size += BlockKindTitle::total_size();
     if size < needed_size {
       return Err(BulkError::TooSmallForTitleSuperBlock);
     }
 
+    main_super_block.free_title_ptr = needed_size;
+
     needed_size += BlockKindData::total_size();
     if size < needed_size {
       return Err(BulkError::TooSmallForDataSuperBlock);
     }
+
+    main_super_block.free_data_ptr = needed_size;
+
+    let main_super_block_bytes = bincode::serialize(&main_super_block);
+    match main_super_block_bytes {
+      Ok(bytes) => {
+        for (i, byte) in bytes.into_iter().enumerate() {
+          buf[i] = byte;
+        }
+      }
+      Err(_) => todo!(),
+    }
+
+    self.inner.write_all(&buf)?;
 
     Ok(())
   }
